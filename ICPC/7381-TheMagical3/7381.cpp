4fcda4d9077ce27158f40f1591d3ecb84ad4ea21
Nikolay <nikecha96@gmail.com>
2017-09-16 22:12:28 -0700

    Optimized 7381

---
+++ ICPC/7381-TheMagical3/7381.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+
+void Solution(int n);
+int findFirstDivisor(int n);
+
+int main() {
+	int n;
+	while (std::cin >> n) {
+		Solution(n);
+	}
+}
+
+void Solution(int n) {
+	if (n == 3) { std::cout << 4 << '\n'; return; }
+	if (n < 7) { std::cout << "No such base\n"; return; }
+	int firstDiv = findFirstDivisor(n-3);
+	std::cout << firstDiv << '\n';
+}
+
+int findFirstDivisor(int n) {
+/*
+Finds the first divisor of a given number n
+*/
+	int endPoint = n / 2;
+	if (n > 20) endPoint = sqrt(n);
+	for (int i = 4; i <= endPoint; i++) {
+		if (! (n%i) ) return i;
+	}
+	return n;
+}
