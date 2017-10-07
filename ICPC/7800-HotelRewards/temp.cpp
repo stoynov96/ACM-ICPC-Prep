2147d5b6059ec0b51a6e23e47391f86c4c45741b
Nikolay <nikecha96@gmail.com>
2017-10-07 09:42:45 -0700

    Added a readme
    Added a readme to signal that not all edge cases are addressed to allow
    for merging with the master branch.

--- ICPC/7800-HotelRewards/temp.cpp
+++ ICPC/7800-HotelRewards/temp.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-	std::cout << 10000 << " " << 20000 << std::endl;
-	for (int i = 1; i <= 10000; i++) {
-		std::cout << i << std::endl;
+	std::cout << 100000 << " " << 20000 << std::endl;
+	for (int i = 1; i <= 100000; i++) {
+		std::cout << 10000 << std::endl;
 	}
 }
