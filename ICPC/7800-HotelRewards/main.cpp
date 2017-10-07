d668125848d4fb75c8e98008dfb418c50f40d238
Nikolay <nikecha96@gmail.com>
2017-10-07 00:42:50 -0700

    New Implementation
    Massively reduced complexity from O(2^(n/k)) to O(n^2/k)

--- ICPC/7800-HotelRewards/main.cpp
+++ ICPC/7800-HotelRewards/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <algorithm>
-#include <climits>
+#include <vector>
 
 using namespace std;
 
@@ -18,27 +17,51 @@ public:
 	*/
 		int i = 0;
 		int price;
-		int *prices = new int[n+1];
-		prices[n] = 0;	// set the last price to 0 to signal end of array (0 is an invalid value for Pi)
+		int *prices = new int[n];
+
+		ll sm = 0, smAll = 0;
+
+		vector <int> lst;
 
 		// Input prices
-		for (i = 0; i < n; ++i)
+		smAll = 0;	// TODO: remove. redundant
+		for (i = 0; i < n; ++i) {
 			cin >> prices[i];
+			smAll += prices[i];
+		}
 		
-		cout << minCost(n, k, (ll)prices[0], 1, prices+1);
+		for (i = k; i < n; ++i) {
+			if ( ! ((i+1) % (k+1)) ) {
+				lst.insert(lst.begin(), 0);
+			}
+			// cout << i << ": " << lst.size() << endl;	// debug
+			// cout << "  lst= [ "; for (int j = 0; j < lst.size(); j++) {cout << lst[j] << " ";} cout << "]" << endl;	// debug
+			if (prices[i] > lst[0]) {
+				insert(lst, prices[i]);
+			}
+			// cout << "  lst= [ "; for (int j = 0; j < lst.size(); j++) {cout << lst[j] << " ";} cout << "]" << endl;	// debug
+		}
 
-
-		// Deallocate memory
-		delete [] prices;
+		sm = 0;	// TODO: remove. redundant
+		for (i = 0; i < lst.size(); ++i) {
+			sm += lst[i];
+		}
+		// cout << "DEBUG: " << smAll << " " << sm << endl; 	//debug
+		cout << smAll - sm;
 	}
-	static ll minCost(ll n, ll k, ll rSum, int currPoints, int *prices) {
-	/*
-	TODO: Add description
-	*/
-		if (currPoints < 0) return ULLONG_MAX;
-		cout << *prices << ":\trSum= " << rSum << " \tp= " << currPoints << "\n";
-		if (!*prices) return rSum;
-		return min (minCost(n, k, rSum+prices[0], currPoints+1, prices+1), minCost(n, k, rSum, currPoints-k, prices+1));
+	static void insert(vector<int> &lst, int num) {
+		int ind = binS(lst, num, 0, lst.size()-1);
+		// cout << "  ind = " << ind << endl;	// debug
+		for (int i = 0; i < ind; ++i) {
+			lst[i] = lst[i+1];
+		}
+		lst[ind] = num;
+	}
+	static int binS(vector<int> &lst, int num, int l, int r) {
+		if (l >= r) return l;
+		int m = (l + r + 1) / 2;
+		if (num < lst[m]) return binS(lst, num, l, m-1);
+		return binS(lst, num, m, r);
 	}
 };
 
